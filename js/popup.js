65e2b2a7640079bb44410cb02342c0a65f3122b6
andyermkv <andreystr93@rambler.ru>
2017-05-29 11:05:01 +0500

    ✏ main page
    Improved main page and added js

---
+++ js/popup.js
@@ -0,0 +1,69 @@
+		/* Попап Логин и Пароль */
+
+		var link = document.querySelector(".login");
+		var popup = document.querySelector(".modal-content");
+		var close = popup.querySelector(".modal-content-close");
+		var login = popup.querySelector("[name=login]");
+		var form = popup.querySelector("form");
+		var password = popup.querySelector("[name=password]");
+		var storage = localStorage.getItem("login");
+		
+		link.addEventListener("click", function(event) {
+			event.preventDefault();
+			popup.classList.add("modal-content-show");
+			login.focus();
+			
+			if (storage) {
+				login.value = storage;
+				password.focus();
+			} else {
+				login.focus();
+			}
+		});
+		
+		close.addEventListener("click", function(event) {
+			event.preventDefault();
+			popup.classList.remove("modal-content-show");
+			popup.classList.remove("modal-error")
+		});
+		
+		form.addEventListener("submit", function(event){
+			if (!login.value || !password.value) {
+				event.preventDefault();
+				popup.classList.add("modal-error");
+			} else {
+				localStorage.setItem("login", login.value);
+			}
+		});
+		
+		window.addEventListener("keydown", function(event) {
+			if (event.keyCode === 27) {
+				if(popup.classList.contains("modal-content-show")) {
+					popup.classList.remove("modal-content-show");
+				}
+			}
+		});
+
+		/* Попап Карта */
+
+		var mapOpen = document.querySelector(".js-open-map");
+		var mapPopup = document.querySelector(".modal-content-map");
+		var mapClose = popup.querySelector(".modal-content-close");
+		
+		mapOpen.addEventListener("click", function(event) {
+			event.preventDefault();
+			mapPopup.classList.add("modal-content-show");
+		});
+		
+		mapClose.addEventListener("click", function(event) {
+			event.preventDefault();
+			mapPopup.classList.remove("modal-content-show");
+		});
+		
+		window.addEventListener("keydown", function(event) {
+			if (event.keyCode === 27) {
+				if(mapPopup.classList.contains("modal-content-show")) {
+					mapPopup.classList.remove("modal-content-show");
+				}
+			}
+		});
